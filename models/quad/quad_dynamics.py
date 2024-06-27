a07bfe6b8479c12d7aad18e959073d60adec0219
Dean Anderson <dben1182@gmail.com>
2024-06-26 21:55:13 -0600

    working on the dynamics file

--- models/quad/quad_dynamics.py
+++ models/quad/quad_dynamics.py
@@ -12,7 +12,7 @@ from tools.quaternions import *
 from message_types.msg_convert import *
 from message_types.quad.msg_state import MsgState
 from message_types.msg_sensors import MsgSensors
-from message_types.msg_delta import MsgDelta
+from message_types.quad.msg_delta import MsgDelta
 
 
 #creates the QuadDynamics class
@@ -47,7 +47,98 @@ class QuadDynamics:
         self._Va = 0.0
         self._alpha = 0.0
         self._beta = 0.0
-        self._update_velocity_data(wind=self._wind)
+        self._update_velocity_data()
+
+        #stores the forces
+        self._forces = np.array([[0.0], [0.0], [0.0]])
+        #initializes the true state message
+        self.true_state = MsgState()
+        #initializes the sensors message
+        self._sensors = MsgSensors()
+        # random walk parameters for GPS
+        self._gps_eta_n = 0.
+        self._gps_eta_e = 0.
+        self._gps_eta_h = 0.
+        # timer so that gps only updates every ts_gps seconds
+        self._t_gps = 999.  # large value ensures gps updates at initial time.
+
+
+    #creates the update function for the system
+    def update(self, delta: MsgDelta, wind: np.ndarray):
+
+
+    #creates the sensors function
+    def sensors(self)->MsgSensors:
+        '''
+            Return value of sensors on MAV: 
+                gyros, 
+                accels, 
+                static_pressure, 
+                dynamic_pressure, 
+                GPS
+        '''
+        phi, theta, psi = quaternion_to_euler(self._state[6:10])
+        pdot = quaternion_to_rotation(self._state[6:10]) @ self._state[3:6]
+        # simulate rate gyros(units are rad / sec)
+        self._sensors.gyro_x = self._state.item(10) \
+            + np.random.normal(SENSOR.gyro_x_bias, SENSOR.gyro_sigma)
+        self._sensors.gyro_y = self._state.item(11) \
+            + np.random.normal(SENSOR.gyro_y_bias, SENSOR.gyro_sigma)
+        self._sensors.gyro_z = self._state.item(12) \
+            + np.random.normal(SENSOR.gyro_z_bias, SENSOR.gyro_sigma)
+        # simulate accelerometers(units of g)
+        self._sensors.accel_x = self._forces.item(0)/QUAD.mass \
+            + QUAD.gravity*np.sin(theta) \
+            + np.random.normal(0., SENSOR.accel_sigma)
+        self._sensors.accel_y = self._forces.item(1)/QUAD.mass \
+            - QUAD.gravity*np.cos(theta)*np.sin(phi) \
+            + np.random.normal(0., SENSOR.accel_sigma)
+        self._sensors.accel_z = self._forces.item(2)/QUAD.mass \
+            - QUAD.gravity*np.cos(theta)*np.cos(phi) \
+            + np.random.normal(0., SENSOR.accel_sigma)
+        # simulate magnetometers
+        # magnetic field in provo has magnetic declination of 12.5 degrees
+        # and magnetic inclination of 66 degrees
+        R_mag = euler_to_rotation(0.0, np.radians(-66), np.radians(12.5))
+        # magnetic field in inertial frame: unit vector
+        mag_inertial = R_mag.T @ np.array([[1.0], [0.0], [0.0]])
+        R = quaternion_to_rotation(self._state[6:10]) # body to inertial
+        # magnetic field in body frame: unit vector
+        mag_body = R.T @ mag_inertial
+        self._sensors.mag_x = mag_body.item(0) \
+            + np.random.normal(0., SENSOR.mag_sigma)
+        self._sensors.mag_y = mag_body.item(1) \
+            + np.random.normal(0., SENSOR.mag_sigma)
+        self._sensors.mag_z = mag_body.item(2) \
+            + np.random.normal(0., SENSOR.mag_sigma)
+        # simulate pressure sensors
+        self._sensors.static_pressure = \
+            -QUAD.rho*QUAD.gravity*self._state.item(2) \
+            + np.random.normal(0., SENSOR.static_pres_sigma)
+        self._sensors.diff_pressure = 0.5 * QUAD.rho * self._Va**2 \
+            + np.random.normal(0., SENSOR.diff_pres_sigma)
+        # simulate GPS sensor
+        if self._t_gps >= SENSOR.ts_gps:
+            self._gps_eta_n = \
+                np.exp(-SENSOR.gps_beta*SENSOR.ts_gps)*self._gps_eta_n \
+                + np.random.normal(0., SENSOR.gps_n_sigma)
+            self._gps_eta_e = \
+                np.exp(-SENSOR.gps_beta*SENSOR.ts_gps)*self._gps_eta_e \
+                + np.random.normal(0., SENSOR.gps_e_sigma)
+            self._gps_eta_h = \
+                np.exp(-SENSOR.gps_beta*SENSOR.ts_gps)*self._gps_eta_h \
+                + np.random.normal(0., SENSOR.gps_h_sigma)
+            self._sensors.gps_n = self._state.item(0) + self._gps_eta_n
+            self._sensors.gps_e = self._state.item(1) + self._gps_eta_e
+            self._sensors.gps_h = -self._state.item(2) + self._gps_eta_h
+            self._sensors.gps_Vg = np.linalg.norm(self._state[3:6]) \
+                + np.random.normal(0., SENSOR.gps_Vg_sigma)
+            self._sensors.gps_course = np.arctan2(pdot.item(1), pdot.item(0)) \
+                + np.random.normal(0., SENSOR.gps_course_sigma)
+            self._t_gps = 0.
+        else:
+            self._t_gps += self._ts
+        return self._sensors
 
 
 
@@ -176,3 +267,231 @@ class QuadDynamics:
             p_nondim = 0.0
             q_nondim = 0.0
             r_nondim = 0.0
+
+        # compute Lift and Drag coefficients
+        tmp1 = np.exp(-QUAD.M * (self._alpha - QUAD.alpha0))
+        tmp2 = np.exp(QUAD.M * (self._alpha + QUAD.alpha0))
+        sigma = (1 + tmp1 + tmp2) / ((1 + tmp1) * (1 + tmp2))
+
+        CL = (1 - sigma) * (QUAD.C_L_0 + QUAD.C_L_alpha * self._alpha) \
+             + sigma * 2 * np.sign(self._alpha) * sa**2 * ca
+        CD = (1 - sigma) \
+            * (QUAD.C_D_p + \
+               ((QUAD.C_L_0 + QUAD.C_L_alpha * self._alpha)**2)\
+                /(np.pi * QUAD.e * QUAD.AR)) \
+            + sigma * 2 * np.sign(self._alpha) * sa
+
+        # compute Lift and Drag Forces
+        F_lift = qbar * QUAD.S_wing * (CL + QUAD.C_L_q * q_nondim + QUAD.C_L_delta_e * elevator)
+        F_drag = qbar * QUAD.S_wing * (CD + QUAD.C_D_q * q_nondim + QUAD.C_D_delta_e * elevator)
+
+        # compute longitudinal forces in body frame
+        fx += - ca * F_drag + sa * F_lift
+        fz += - sa * F_drag - ca * F_lift
+        # compute lateral forces in body frame
+        fy += qbar * QUAD.S_wing * (
+                  QUAD.C_Y_0
+                + QUAD.C_Y_beta * self._beta
+                + QUAD.C_Y_p * p_nondim
+                + QUAD.C_Y_r * r_nondim
+                + QUAD.C_Y_delta_a * aileron
+                + QUAD.C_Y_delta_r * rudder)
+        
+        # compute logitudinal torque in body frame
+        My = qbar * QUAD.S_wing * QUAD.c * (
+                QUAD.C_m_0
+                + QUAD.C_m_alpha * self._alpha
+                + QUAD.C_m_q * q_nondim
+                + QUAD.C_m_delta_e * elevator
+        )
+        # compute lateral torques in body frame
+        Mx  = qbar * QUAD.S_wing * QUAD.b * (
+                QUAD.C_ell_0
+                + QUAD.C_ell_beta * self._beta
+                + QUAD.C_ell_p * p_nondim
+                + QUAD.C_ell_r * r_nondim
+                + QUAD.C_ell_delta_a * aileron
+                + QUAD.C_ell_delta_r * rudder
+        )
+        Mz = qbar * QUAD.S_wing * QUAD.b * (
+                QUAD.C_n_0 + QUAD.C_n_beta * self._beta
+                + QUAD.C_n_p * p_nondim
+                + QUAD.C_n_r * r_nondim
+                + QUAD.C_n_delta_a * aileron
+                + QUAD.C_n_delta_r * rudder
+        )
+        
+
+        #############################################################################################################
+        #Forces and moments from the forward prop
+
+        #gets the airspeed through the rear, main propulsion propellor
+        Va_forward_prop = np.array([[1.0], [0.0], [0.0]]).T @ self.v_air
+
+        #gets the thrust and the moment from the forward prop
+        Thrust_Forward, Prop_Moment_Forward = self._motor_thrust_torque(Va_forward_prop, delta.forwardThrottle)
+
+        #gets the force on the airplane from the forward propeller
+        #In this case, it is in the positive x direction, so we multiply by the unit vector in the direction the thrust is facing
+        Force_Forward = Thrust_Forward * np.array([[1.0], [0.0], [0.0]])
+
+        #gets the moment, which is a little more complicated
+        ##############################*******************************************************************************************
+        #I may need to change this to account for the direction of the prop rotation
+        #I am currently choosing it to rotate clockwise, when at the back, looking to the front of the airplane
+        Moment_Forward = Prop_Moment_Forward*np.array([[1.0],[0.0],[0.0]]) + np.cross(QUAD.forward_rotor_pos.T, Force_Forward.T).T
+
+        #adds each component to the whole forces and moment variables
+        fx += Force_Forward.item(0)
+        fy += Force_Forward.item(1)
+        fz += Force_Forward.item(2)
+
+        Mx += Moment_Forward.item(0)
+        My += Moment_Forward.item(1)
+        Mz += Moment_Forward.item(2)
+        #############################################################################################################
+
+
+
+        #############################################################################################################
+        #Forces and moments from the rear port propeller
+        Va_rear_port_prop = np.array([[0.0],[0.0],[-1.0]]).T @ self.v_air
+
+        #gets the thrust and the moment from the rear port propeller
+        Thrust_RearPort, Prop_Moment_RearPort = self._motor_thrust_torque(Va_rear_port_prop, delta.verticalThrottle_1)
+
+        #gets the RearPort Force
+        Force_RearPort = Thrust_RearPort*np.array([[0.0],[0.0],[-1.0]])
+
+        #gets the rear port moment
+        Moment_RearPort = Prop_Moment_RearPort*np.array([[0.0],[0.0],[-1.0]]) + np.cross(QUAD.vertical_rotor_1_pos.T, Force_RearPort.T).T
+
+        #adds each component to the whole forces and moment variables
+        fx += Force_RearPort.item(0)
+        fy += Force_RearPort.item(1)
+        fz += Force_RearPort.item(2)
+
+        Mx += Moment_RearPort.item(0)
+        My += Moment_RearPort.item(1)
+        Mz += Moment_RearPort.item(2)
+        #############################################################################################################
+
+        #############################################################################################################
+        #Forces and moments from the front port propeller
+        Va_front_port_prop = np.array([[0.0],[0.0],[-1.0]]).T @ self.v_air
+
+        #gets the thrust and the moment from the rear port propeller
+        Thrust_FrontPort, Prop_Moment_FrontPort = self._motor_thrust_torque(Va_front_port_prop, delta.verticalThrottle_2)
+
+        #gets the RearPort Force
+        Force_FrontPort = Thrust_FrontPort*np.array([[0.0],[0.0],[-1.0]])
+
+        #gets the rear port moment
+        Moment_FrontPort = Prop_Moment_FrontPort*np.array([[0.0],[0.0],[-1.0]]) + np.cross(QUAD.vertical_rotor_2_pos.T, Force_FrontPort.T).T
+
+        #adds each component to the whole forces and moment variables
+        fx += Force_FrontPort.item(0)
+        fy += Force_FrontPort.item(1)
+        fz += Force_FrontPort.item(2)
+
+        Mx += Moment_FrontPort.item(0)
+        My += Moment_FrontPort.item(1)
+        Mz += Moment_FrontPort.item(2)
+
+        #############################################################################################################
+
+        #############################################################################################################
+        #Forces and moments from the front starboard propeller
+        Va_front_starboard_prop = np.array([[0.0],[0.0],[-1.0]]).T @ self.v_air
+
+        #gets the thrust and the moment from the rear port propeller
+        Thrust_FrontStarboard, Prop_Moment_FrontStarboard = self._motor_thrust_torque(Va_front_starboard_prop, delta.verticalThrottle_3)
+
+        #gets the RearStarboard Force
+        Force_FrontStarboard = Thrust_FrontStarboard*np.array([[0.0],[0.0],[-1.0]])
+
+        #gets the rear starboard moment
+        Moment_FrontStarboard = Prop_Moment_FrontStarboard*np.array([[0.0],[0.0],[-1.0]]) + np.cross(QUAD.vertical_rotor_3_pos.T, Force_FrontStarboard.T).T
+
+        #adds each component to the whole forces and moment variables
+        fx += Force_FrontStarboard.item(0)
+        fy += Force_FrontStarboard.item(1)
+        fz += Force_FrontStarboard.item(2)
+
+        Mx += Moment_FrontStarboard.item(0)
+        My += Moment_FrontStarboard.item(1)
+        Mz += Moment_FrontStarboard.item(2)
+
+        #############################################################################################################
+
+        #############################################################################################################
+        #Forces and moments from the rear starboard propeller
+
+        #Forces and moments from the front starboard propeller
+        Va_rear_starboard_prop = np.array([[0.0],[0.0],[-1.0]]).T @ self.v_air
+
+        #gets the thrust and the moment from the rear port propeller
+        Thrust_RearStarboard, Prop_Moment_RearStarboard = self._motor_thrust_torque(Va_rear_starboard_prop, delta.verticalThrottle_4)
+
+        #gets the RearStarboard Force
+        Force_RearStarboard = Thrust_RearStarboard*np.array([[0.0],[0.0],[-1.0]])
+
+        #gets the rear starboard moment
+        Moment_RearStarboard = Prop_Moment_RearStarboard*np.array([[0.0],[0.0],[-1.0]]) + np.cross(QUAD.vertical_rotor_4_pos.T, Force_RearStarboard.T).T
+
+        #adds each component to the whole forces and moment variables
+        fx += Force_RearStarboard.item(0)
+        fy += Force_RearStarboard.item(1)
+        fz += Force_RearStarboard.item(2)
+
+        Mx += Moment_RearStarboard.item(0)
+        My += Moment_RearStarboard.item(1)
+        Mz += Moment_RearStarboard.item(2)
+
+        #############################################################################################################
+
+        #returns the forces
+        return np.array([[fx, fy, fz, Mx, My, Mz]]).T
+
+
+    
+    def _motor_thrust_torque(self, Va: float, delta_t: float)->tuple[float, float]:
+
+
+        #gets the coefficients for the propeller
+
+        C_Q0 = QUAD.C_Q0
+        C_Q1 = QUAD.C_Q1
+        C_T0 = QUAD.C_T0
+        C_Q2 = QUAD.C_Q2
+        C_T1 = QUAD.C_T1
+        C_T2 = QUAD.C_T2
+        D_prop = QUAD.D_prop
+        KQ = QUAD.KQ
+        R_motor = QUAD.R_motor
+        i0 = QUAD.i0
+
+
+        #gets the voltage in, based on the delta_t
+        V_in = QUAD.V_max * delta_t
+        # Quadratic formula to solve for motor speed
+        a = C_Q0 * QUAD.rho * np.power(D_prop, 5) \
+            / ((2.*np.pi)**2)
+        b = (C_Q1 * QUAD.rho * np.power(D_prop, 4)
+             / (2.*np.pi)) * Va + KQ**2/R_motor
+        c = C_Q2 * QUAD.rho * np.power(D_prop, 3) \
+            * Va**2 - (KQ / R_motor) * V_in + KQ * i0        
+        # Consider only positive root
+        Omega_op = (-b + np.sqrt(b**2 - 4*a*c)) / (2.*a)
+        # compute advance ratio
+        J_op = 2 * np.pi * Va / (Omega_op * D_prop)
+        # compute non-dimensionalized coefficients of thrust and torque
+        C_T = C_T2 * J_op**2 + C_T1 * J_op + C_T0
+        C_Q = C_Q2 * J_op**2 + C_Q1 * J_op + C_Q0
+        # add thrust and torque due to propeller
+        n = Omega_op / (2 * np.pi)
+        T_p = QUAD.rho * n**2 * np.power(D_prop, 4) * C_T
+        Q_p = QUAD.rho * n**2 * np.power(D_prop, 5) * C_Q
+        return T_p, Q_p
+
+
--- parameters/quad/anaconda_parameters.py
+++ parameters/quad/anaconda_parameters.py
@@ -65,20 +65,81 @@ vertical_rotor_2_pos = np.array([[0.5], [-0.5], [0.0]])
 vertical_rotor_3_pos = np.array([[0.5], [0.5], [0.0]])
 vertical_rotor_4_pos = np.array([[-0.5], [0.5], [0.0]])
 
+#some necessary parameters
+M = 50.0
+alpha0 = np.deg2rad(15) #0.47
+epsilon = 0.16
+
+
+#######################################################################################
+#Coefficients
+C_L_0 = 0.005
+C_D_0 = 0.0022
+C_m_0 = 0.0
+C_L_alpha = 2.819
+C_D_alpha = 0.03
+C_m_alpha = -0.185
+C_L_q = 3.242
+C_D_q = 0.0
+C_m_q = -1.093
+C_L_delta_e = 0.2
+C_D_delta_e = 0.005
+C_m_delta_e = -0.05
+
+C_Y_0 = 0.0
+C_ell_0 = 0.0
+C_n_0 = 0.0
+
+C_Y_beta = -0.318
+C_ell_beta = -0.032
+C_n_beta = 0.112
+C_Y_p = 0.078
+C_ell_p = -0.207
+C_n_p = -0.053
+C_Y_r = 0.288
+C_ell_r = 0.036
+C_n_r = -0.104
+C_Y_delta_a = 0.000536
+C_ell_delta_a = 0.018
+C_n_delta_a = -0.00328#-0.011
+C_Y_delta_r = 0.0
+C_ell_delta_r = 0.0
+C_n_delta_r = 0.0
 
 
 
+######################################################################################
+                #  Propeller parameters
+######################################################################################
+
+# # Prop parameters
+D_prop = 20*(0.0254)     # prop diameter in m
 
 
+# Motor parameters
+K_V = 145.                   # from datasheet RPM/V
+KQ = (1. / K_V) * 60. / (2. * np.pi)  # KQ in N-m/A, V-s/rad
+R_motor = 0.042              # ohms
+i0 = 1.5                     # no-load (zero-torque) current (A)
+
+k_force = 65.0
+k_moment = 5.0
+
+# Inputs
+# ncells = 12.
+# V_max = 3.7 * ncells  # max voltage for specified number of battery cells
+# Inputs
+ncells = 3.
+V_max = 3.7 * ncells  # max voltage for specified number of battery cells
 
 
-
-
-
-
-
-
-
+# Coeffiecients from prop_data fit
+C_Q2 = -0.01664
+C_Q1 = 0.004970
+C_Q0 = 0.005230
+C_T2 = -0.1079
+C_T1 = -0.06044
+C_T0 = 0.09357
 
 
 
