2e14c14dcc66f6ac207d9de184ead01f2ea364db
Dean Anderson <dben1182@gmail.com>
2024-08-09 17:34:21 -0600

    update on LQR

--- controllers/quad/lqr/lqr_control.py
+++ controllers/quad/lqr/lqr_control.py
@@ -1,3 +1,45 @@
 import numpy as np
 import scipy
 from message_types.msg_convert import *
+
+
+
+#creates the Lqr control class
+class LqrControl:
+
+    #creates the initialization fnction
+    def __init__(self, Ts):
+        self.Ts = Ts
+
+        #saves the number of actuators
+        numActuators = 8
+        #stores the previous input
+        self.u_prev = np.zeros((numActuators, 1))
+        #saves the alpha
+        self.alpha = 0.5
+        #saves the epsilon
+        self.epsilon = 0.001
+
+        #creates the Q matrix
+        self.Q = np.diag([1/10.0, # north position
+                          1/10.0, # east position
+                          1/10.0, # down position
+                          1/10.0, # x body velocity
+                          1/10.0, # y body velocity
+                          1/10.0, # z body velocity
+                          2.0,    # q_tilde 0
+                          2.0,    # q_tilde 1
+                          2.0])   # q_tilde 2
+        
+        #creates the R matrix
+        self.R = np.diag([1.0,
+                          1.0,
+                          1.0,
+                          1.0,
+                          1.0,
+                          1.0,
+                          1.0,
+                          1.0])
+        
+    #creates the update function
+    def update(self, x, x_desired, u_desired, df_traj):
