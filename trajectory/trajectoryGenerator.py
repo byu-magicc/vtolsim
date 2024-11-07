994bf45cbe4747c79ec1d4c982758ab434507e46
Dean Anderson <dben1182@gmail.com>
2024-11-07 15:55:08 -0700

    update

--- trajectory/trajectoryGenerator.py
+++ trajectory/trajectoryGenerator.py
@@ -1,1 +1,18 @@
 #This file implements the generators that create the trajectories for plane
+
+
+#creates the trajectory generator class
+class TrajectoryGenerator():
+
+    #creates the initialization function
+    def __init__(self):
+        #creates the list for the segments
+        self.segments = []
+        #initializes the end time for the simulation to be zero
+        self.end_time = 0.0
+
+    #
+
+
+
+#I suppose that we should create a libaray that is versatile to all sorts of 
